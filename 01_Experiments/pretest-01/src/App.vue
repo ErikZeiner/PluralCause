3e69b29030e8d48b5dcf9ecfa56154442bd5da33
Erik Zeiner <47943924+ErikZeiner@users.noreply.github.com>
2026-05-25 14:40:32 +0200

    add fixes

--- 01_Experiments/pretest-01/src/App.vue
+++ 01_Experiments/pretest-01/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <Experiment title="Plural Cause Pretest 01">
     <InstructionScreen :title="'Welcome'">
-      In this experiment you will ask for your judgement of several statements based on a fictional scenario.
+      In this study, you will be presented with a fictional scenario and asked for your judgement of a sentence.
     </InstructionScreen>
 
     <InstructionScreen>
@@ -14,158 +14,111 @@
       <p>
         This route can only officially be established with approval from the <b>Interplanetary Trade Council of Planets
         Luteus and Viridis</b>.
-        Each planet has a delegate and <b>both must approve</b> the route for it to be authorised.
+        Each planet has a representative and <b>both must approve</b> the route for it to be authorised.
       </p>
 
       <img src="images/council.png"/>
     </InstructionScreen>
 
-
     <template v-for="(trial, i) in comprehension_trials">
-      <Screen>
+      <Screen :key="'comprehension-' + i">
         <Slide>
-          <span style='color:grey'>Remember that the route can only be established with Council approval meaning both members must agree.</span>
+          <div v-if="($magpie.measurements['attempts_' + i] || 0) < 2">
+            <span style='colour:grey'>Remember that the route can only be established with the approval of the Council, meaning both members must agree.</span>
 
+            <p>
+              <img :src="trial.picture"/>
+            </p>
 
-          <p>
-            <img :src="trial.picture"/>
-          </p>
+            <p>
+              Could the trade route be established given these results?
+            </p>
 
-          <p>
-            Could the trade route be established given these results?
-          </p>
+            <ForcedChoiceInput
+                :response.sync="$magpie.measurements['given_response_' + i]"
+                :options="['Yes, it could.','No, it could not.']"
+                @update:response="check_response($magpie.measurements['given_response_' + i], trial.correct_response, i)"/>
 
-          <ForcedChoiceInput
-              :response.sync="$magpie.measurements.comp_response"
-              :options="['Yes, it could.','No, it could not.']"
-              @update:response="$magpie.saveAndNextScreen();"/>
+            <p v-if="$magpie.measurements['attempts_' + i] === 1" style="colour: #B22222; font-weight: bold;">
+              That is incorrect. Please look at the rules at the top of the screen and try again.
+            </p>
+          </div>
+
+          <div v-else style="text-align: center; padding: 20px;">
+            <p>You have answered the comprehension question incorrectly twice.</p>
+            <p><b>Please close the study and click 'Stop Without Completing' on Prolific to return your submission.</b>
+            </p>
+          </div>
 
           <Record
               :data="{
               trial_type : 'comprehension',
-              trial_num : i+1,
+              trial_num : i,
               scenario : trial.scenario,
-              given_response : $magpie.measurements.given_response,
+              given_response : $magpie.measurements['given_response_' + i],
               correct_response: trial.correct_response,
+              failed_comprehension: ($magpie.measurements['attempts_' + i] || 0) >= 2
             }"
           />
-
         </Slide>
       </Screen>
     </template>
-
+    <InstructionScreen>
+      <p>You have passed all comprehension questions! Now we can move onto the main part of the experiment.</p>
+    </InstructionScreen>
 
     <Screen>
       <Slide>
         <p>
-          <p>The day when the new trade route gets decided on finally comes and this is the result:</p>
-          <img src="images/council_yes-yes.png"/>
-          <p>An inhabitant of a third planet in the solar system, Glaucus, describes the result like this:
-          </p>
-          <img :src="granularity === 'coarse' ? 'images/glaucusian_coarse.png' : 'images/glaucusian_fine.png'"/>
+          The long-awaited day of deciding the new trade route finally arrives and here’s the result:
+        </p>
+        <img src="images/council_yes-yes.png"/>
+        <p>An inhabitant of Glaucus, the third planet in this solar system, describes the result like this:
+        </p>
+        <img :src="granularity === 'coarse' ? 'images/glaucusian_coarse.png' : 'images/glaucusian_fine.png'"/>
 
-          <Button v-if="!show_singular"
-                  @click="show_singular = true">
-            Next
-          </Button>
-          <p v-if="show_singular">
-            How likely is that inhabitant of Glaucus to agree with the following statement?
-          </p>
-          <p v-if="show_singular">
-            <b>The route can be established because the representative from {{ singular_cause }} agreed.</b>
-            <RatingInput
-                left="XXX"
-                right="YYY"
-                :response.sync="$magpie.measurements.singular_response"
-            />
-          </p>
+        <Button v-if="!show_singular"
+                @click="show_singular = true">
+          Next
+        </Button>
+        <p v-if="show_singular">
+          How likely is it that this individual would also agree with the following statement?
+        </p>
+        <p v-if="show_singular">
+          <b>The route can be established because the representative from {{ singular_cause }} agreed to it.</b>
+          <RatingInput
+              left="Very unlikely"
+              right="Very likely"
+              :response.sync="$magpie.measurements.singular_response"
+          />
+        </p>
 
-          <p v-if="$magpie.measurements.singular_response > 0">
-            <button @click="$magpie.saveAndNextScreen();">Submit</button>
-          </p>
+        <p v-if="$magpie.measurements.singular_response > 0">
+          <button @click="$magpie.saveAndNextScreen();">Submit</button>
+        </p>
 
-          <Record
-              :data="{
+        <Record
+            :data="{
               trial_type : 'critical',
-              trial_num : 1,
               granularity : granularity,
               singular_cause : singular_cause,
-              // plural_response : $mapgie.measurements.plural_response,
-              // singular_response : $magpie.measurements.singular_response
             }"
-          />
+        />
       </Slide>
     </Screen>
-    <!-- Speaker task template-->
-    <!--    <Screen>-->
-    <!--      <Slide>-->
-    <!--        <p>-->
-    <!--          The route is established because-->
-    <!--          {{-->
-    <!--            granularity === 'coarse' ? 'the Interplanetary Trade Council of planets Luteus and Viridis agreed.' : 'the representative of Luteus and the representative of Viridis agreed.'-->
-    <!--          }}-->
-
-    <!--          <div :style="{-->
-    <!--          opacity: !show_singular ? 1 : 0.4,-->
-    <!--          pointerEvents: !show_singular ? 'auto' : 'none'-->
-    <!--          }">-->
-    <!--            <RatingInput-->
-    <!--                left="XXX"-->
-    <!--                right="YYY"-->
-    <!--                :response.sync="$magpie.measurements.plural_response"-->
-    <!--            />-->
-    <!--          </div>-->
-    <!--        </p>-->
-    <!--        <Button v-if="!show_singular"-->
-    <!--                @click="show_singular = true">-->
-    <!--          Next-->
-    <!--        </Button>-->
-    <!--        <p v-if="show_singular">-->
-
-    <!--        </p>-->
-    <!--        <p v-if="show_singular">-->
-    <!--          The route can be established because the representative from {{ singular_cause }} agreed.-->
-    <!--          <RatingInput-->
-    <!--              left="XXX"-->
-    <!--              right="YYY"-->
-    <!--              :response.sync="$magpie.measurements.singular_response"-->
-    <!--          />-->
-    <!--        </p>-->
-
-    <!--        <p v-if= "$magpie.measurements.singular_response > 0">-->
-    <!--          <button @click="$magpie.saveAndNextScreen();">Submit</button>-->
-    <!--        </p>-->
-
-    <!--        <Record-->
-    <!--            :data="{-->
-    <!--              trial_type : 'critical',-->
-    <!--              trial_num : 1,-->
-    <!--              granularity : granularity,-->
-    <!--              singular_cause : singular_cause,-->
-    <!--              // plural_response : $mapgie.measurements.plural_response,-->
-    <!--              // singular_response : $magpie.measurements.singular_response-->
-    <!--            }"-->
-    <!--        />-->
-    <!--      </Slide>-->
-    <!--    </Screen>-->
 
     <SubmitResultsScreen/>
   </Experiment>
 </template>
 
 <script>
+import Vue from 'vue';
 import _ from 'lodash';
-
-const granularity = _.sample(["coarse", "fine"])
-console.log(granularity);
-
-const singular_cause = _.sample(["Viridis", "Luteus"]);
-console.log(singular_cause);
-
 import comprehension_trials_ordered from './comprehension_trials.csv';
 
+const granularity = _.sample(["coarse", "fine"]);
+const singular_cause = _.sample(["Viridis", "Luteus"]);
 const comprehension_trials = _.shuffle(comprehension_trials_ordered);
-console.log(JSON.parse(JSON.stringify(comprehension_trials_ordered)));
 
 export default {
   name: 'App',
@@ -175,13 +128,27 @@ export default {
       singular_cause: singular_cause,
       comprehension_trials: comprehension_trials,
       show_singular: false,
-
     };
   },
   computed: {
-    // Expose lodash to template code
     _() {
       return _;
+    }
+  },
+  methods: {
+    check_response(given_response, correct_response, index) {
+      if (this.$magpie.measurements['attempts_' + index] === undefined) {
+        Vue.set(this.$magpie.measurements, 'attempts_' + index, 0);
+      }
+      if (given_response === correct_response) {
+        this.$magpie.saveAndNextScreen();
+      } else {
+        this.$magpie.measurements['attempts_' + index] += 1;
+
+        if (this.$magpie.measurements['attempts_' + index] < 2) {
+          this.$magpie.measurements['given_response_' + index] = null;
+        }
+      }
     }
   }
 };
